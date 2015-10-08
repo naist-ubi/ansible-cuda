a7239e7dafb4b6e3769d3755a48bebdd292e89f0
Yosuke Tamura <tamura.yosuke.tp8@gmail.com>
2015-10-08 16:19:15 +0900

    add env vars

--- cuda.sh
+++ cuda.sh
@@ -1,2 +1,5 @@
-export PATH=/usr/local/cuda-7.5/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-7.5/lib64:$LD_LIBRARY_PATH
+export CUDA_ROOT=/usr/local/cuda
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export CPATH=$CPATH:/usr/local/cuda/include
+export CUDA_INC_DIR=/usr/local/cuda/bin:$CUDA_INC_DIR
